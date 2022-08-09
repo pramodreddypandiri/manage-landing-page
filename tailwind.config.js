a07d06aaebd97475b74daa3f4045bbba24d3f8f4
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-08-09 22:23:03 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'brightRed':'#f25f3a',
+        'darkBlue':'#242d52',
+      }
+      
+    },
+    
+  },
+  plugins: [],
+}
